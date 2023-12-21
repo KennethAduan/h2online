4125d76789c25b014afed3189974ff7975aac22c
janrafael.guno <jan.guno@diversifyoss.com>
2023-12-22 00:33:49 +0800

    nothing much

--- src/pages/POS/PayButton/PayButtonOrder.tsx
+++ src/pages/POS/PayButton/PayButtonOrder.tsx
@@ -1,13 +1,15 @@
 import { Button } from "@material-tailwind/react";
 import Swal from "sweetalert2";
-import { useAppSelector } from "../../../utils/redux/hooks";
+import { useAppSelector, useAppDispatch } from "../../../utils/redux/hooks";
 import { AddPurchaseOrderFirebase } from "../../../firebase/services";
+import { clearOrder } from "../../../utils/redux/slice/orderSlice";
+
 const PayButtonOrder = () => {
   // Redux Items
   const items = useAppSelector((state) => state.order.items);
   const { totalAmount } = useAppSelector((state) => state.order);
   const itemNumber = items.length;
-
+  const dispatch = useAppDispatch();
   //   console.log("Items:", items);
   const handlePay = () => {
     Swal.fire({
@@ -21,11 +23,15 @@ const PayButtonOrder = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         await AddPurchaseOrderFirebase(items, totalAmount, itemNumber);
+        // Di ako marunong mag redux so eto muna
+        // if (isSuccess){
+          dispatch(clearOrder());
         Swal.fire({
           title: "Success",
           text: "Payment has been made",
           icon: "success",
         });
+        // }
       }
     });
   };
